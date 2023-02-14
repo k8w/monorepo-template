c47f149f52f151abc2b8f38a0891e46fec4bf744
k8w <me@k8w.cn>
2023-02-14 18:41:44 +0800

    chore: update test case

--- packages/sub1/src/lib/sub1.spec.ts
+++ packages/sub1/src/lib/sub1.spec.ts
@@ -2,6 +2,6 @@ import { sub1 } from './sub1';
 
 describe('sub1', () => {
   it('should work', () => {
-    expect(sub1()).toEqual('sub1');
+    expect(sub1()).toEqual('sub11');
   });
 });
--- packages/sub2/src/lib/sub2.spec.ts
+++ packages/sub2/src/lib/sub2.spec.ts
@@ -2,6 +2,6 @@ import { sub2 } from './sub2';
 
 describe('sub2', () => {
   it('should work', () => {
-    expect(sub2()).toEqual('sub2');
+    expect(sub2()).toEqual('sub22');
   });
 });
